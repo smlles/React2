04ddd40a9cd5cf6e6a7b7c8b80f21620d54b7ace
smlles <yangjjn95@gmail.com>
2025-06-17 17:50:46 +0900

    빌드자동확인3트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
 import {AppBar, Button, Container, Grid, List, Paper, Toolbar, Typography} from '@mui/material'
